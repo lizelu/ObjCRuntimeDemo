f1da75287ed14c099fc7afd997c515f583e415be
zeluliCredit <zeluli@creditease.cn>
2017-01-04 16:40:27 +0800

    project init

---
+++ ObjCRuntimeDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjCRuntimeDemo
+//
+//  Created by Mr.LuDashi on 2017/1/4.
+//  Copyright © 2017年 ZeluLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
